14f8cc7bb90357709b3df4cb47c37aed2159a053
Shivam Singh <shivamsingh4458@gmail.com>
2022-02-15 09:26:14 +0530

    Creating NavBar and Footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand">AG-GridApp</span>
+          <span className="navbar-brand fs-3">Grid Table</span>
+          <span className="navbar-brand">Uraan-App</span>
+        </div>
+      </nav>
+    </>
+  );
+}
